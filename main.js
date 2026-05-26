a09cea03c4f9957aa713938e9005c316e2cc6ecb
Brain <poc008@ucsd.edu>
2026-05-26 16:38:44 -0700

    Refactor D3.js circle and projection settings

--- main.js
+++ main.js
@@ -7,23 +7,67 @@ const svg = d3.select("#globe")
   .attr("width", width)
   .attr("height", height);
 
+const projection = d3.geoOrthographic()
+
+  .scale(300)
+
+  .translate([width / 2, height / 2])
+
+  .rotate([0, -20]);
+
+const path = d3.geoPath(projection);
+
 svg.append("circle")
 
   .attr("cx", width / 2)
 
   .attr("cy", height / 2)
 
-  .attr("r", 280)
+  .attr("r", 300)
 
-  .attr("fill", "#0ea5e9")
-
-  .attr("opacity", 0.25)
-
-  .attr("stroke", "#67e8f9")
-
-  .attr("stroke-width", 4)
+  .attr("fill", "#082f49")
 
   .style(
     "filter",
-    "drop-shadow(0px 0px 40px #38bdf8)"
+    "drop-shadow(0px 0px 50px #38bdf8)"
   );
+
+d3.json(
+  "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
+).then(world => {
+
+  const countries = topojson.feature(
+    world,
+    world.objects.countries
+  );
+
+  svg.selectAll("path")
+
+    .data(countries.features)
+
+    .enter()
+
+    .append("path")
+
+    .attr("d", path)
+
+    .attr("fill", "#14532d")
+
+    .attr("stroke", "#67e8f9")
+
+    .attr("stroke-width", 0.3);
+
+  let rotation = 0;
+
+  d3.timer(() => {
+
+    rotation += 0.05;
+
+    projection.rotate([rotation, -20]);
+
+    svg.selectAll("path")
+      .attr("d", path);
+
+  });
+
+});
