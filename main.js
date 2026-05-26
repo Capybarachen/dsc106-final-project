a54e66794f2ce681efa4b292240b9e4f6852f4a6
Brain <poc008@ucsd.edu>
2026-05-26 16:22:27 -0700

    Refactor SVG chart dimensions and circle attributes

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
-const svg = d3.select("#chart")
+const width = 700;
+
+const height = 700;
+
+const svg = d3.select("#globe")
   .append("svg")
-  .attr("width", 600)
-  .attr("height", 400);
+  .attr("width", width)
+  .attr("height", height);
+
+const projection = d3.geoOrthographic()
+  .scale(300)
+  .translate([width / 2, height / 2]);
+
+const path = d3.geoPath(projection);
 
 svg.append("circle")
-  .attr("cx", 300)
-  .attr("cy", 200)
-  .attr("r", 80)
-  .attr("fill", "orange");
+  .attr("cx", width / 2)
+  .attr("cy", height / 2)
+  .attr("r", 300)
+  .attr("fill", "#0f172a")
+  .attr("stroke", "#38bdf8")
+  .attr("stroke-width", 2);
+
+let rotation = 0;
+
+d3.timer(() => {
+
+  rotation += 0.1;
+
+  projection.rotate([rotation, -15]);
+
+  svg.selectAll("path")
+    .attr("d", path);
+
+});
