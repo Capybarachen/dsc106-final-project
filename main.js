f7a95a6b2552ead39b1e4f3fd0f2b5dc95ed90ce
Brain <poc008@ucsd.edu>
2026-05-26 11:59:49 -0700

    Add SVG chart with a circle element

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const svg = d3.select("#chart")
+  .append("svg")
+  .attr("width", 600)
+  .attr("height", 400);
 
+svg.append("circle")
+  .attr("cx", 300)
+  .attr("cy", 200)
+  .attr("r", 80)
+  .attr("fill", "orange");
