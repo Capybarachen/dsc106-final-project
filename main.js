37ccd4dddfff0da0920059d4a94b999f2264e99e
Brain <poc008@ucsd.edu>
2026-05-31 21:56:33 -0700

    Increase chart dimensions to 2000x700

--- main.js
+++ main.js
@@ -504,8 +504,8 @@ function drawRegionChart(name){
     d3.select("#region-chart")
       .html("");
 
-    const width = 1300;
-    const height = 500;
+    const width = 2000;
+    const height = 700;
 
     const margin = {
         top:30,
