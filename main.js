16c46db62338b25c1eed937822e314fc1d5e4a54
Brain <poc008@ucsd.edu>
2026-05-31 21:55:21 -0700

    Increase chart dimensions to 1300x500

--- main.js
+++ main.js
@@ -504,8 +504,8 @@ function drawRegionChart(name){
     d3.select("#region-chart")
       .html("");
 
-    const width = 650;
-    const height = 400;
+    const width = 1300;
+    const height = 500;
 
     const margin = {
         top:30,
