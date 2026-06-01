98dde9595f3935688626784a2f34af094f62420e
Brain <poc008@ucsd.edu>
2026-05-31 20:50:00 -0700

    Enhance drawRegionChart with D3 visualization
    Refactor drawRegionChart function to include D3 chart rendering.

--- main.js
+++ main.js
@@ -475,13 +475,92 @@ function drawRegionChart(name){
 
     const key =
         name
-          .toLowerCase()
-          .replaceAll(" ", "_");
+        .toLowerCase()
+        .replaceAll(" ","_");
 
     const data =
         regionData[key];
 
-    console.log(data);
+    d3.select("#region-chart")
+      .html("");
+
+    const width = 500;
+    const height = 300;
+
+    const margin = {
+        top:20,
+        right:20,
+        bottom:40,
+        left:50
+    };
+
+    const svg =
+        d3.select("#region-chart")
+          .append("svg")
+          .attr("width",width)
+          .attr("height",height);
+
+    const x =
+        d3.scaleLinear()
+          .domain(
+              d3.extent(
+                  data,
+                  d => d.year
+              )
+          )
+          .range([
+              margin.left,
+              width-margin.right
+          ]);
+
+    const y =
+        d3.scaleLinear()
+          .domain([
+              d3.min(
+                  data,
+                  d => d.tas_anomaly
+              ),
+              d3.max(
+                  data,
+                  d => d.tas_anomaly
+              )
+          ])
+          .range([
+              height-margin.bottom,
+              margin.top
+          ]);
+
+    svg.append("g")
+       .attr(
+           "transform",
+           `translate(0,${
+               height-margin.bottom
+           })`
+       )
+       .call(
+           d3.axisBottom(x)
+       );
+
+    svg.append("g")
+       .attr(
+           "transform",
+           `translate(${margin.left},0)`
+       )
+       .call(
+           d3.axisLeft(y)
+       );
+
+    const line =
+        d3.line()
+          .x(d => x(d.year))
+          .y(d => y(d.tas_anomaly));
+
+    svg.append("path")
+       .datum(data)
+       .attr("fill","none")
+       .attr("stroke","#ff7f50")
+       .attr("stroke-width",3)
+       .attr("d",line);
 
 }
 
